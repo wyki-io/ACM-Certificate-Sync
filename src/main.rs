76582ae2f42cb5874245c45385ff532801c5fa80
Wykiki <jmegaplay@hotmail.fr>
2020-06-12 11:54:36 +0200

    Continue first test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use acs::run;
 
-use futures::{StreamExt, TryStreamExt};
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     run().await
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,15 +2,72 @@ use acs::run;
 
 use k8s_openapi::api::core::v1::Secret;
 use kube::{
-    api::{Api, ListParams},
+    api::{Api, DeleteParams, ListParams, Meta},
     runtime::Informer,
     Client,
 };
 
+// use futures::executor::spawn;
 use futures::{StreamExt, TryStreamExt};
 use futures_await_test::async_test;
 
+use std::path::Path;
+use std::thread;
+
+use serde_yaml::from_reader;
+
+async fn init_client() -> Client {
+    Client::try_default()
+        .await
+        .expect("Unabled to create client")
+}
+
+async fn delete_certificates(client: &Client) {
+    let secrets: Api<Secret> = Api::all(client.clone());
+    let lp = ListParams::default();
+    let secret_list = secrets.list(&lp).await.expect("Unable to list secrets");
+
+    for secret in secret_list {
+        if secret.clone().type_.unwrap_or_default() == "kubernetes.io/tls" {
+            let namespace = Meta::namespace(&secret).unwrap_or(String::from("default"));
+            println!(
+                "Deleting Secret with name {} in namespace {}",
+                Meta::name(&secret),
+                &namespace
+            );
+            let secrets_namespaced: Api<Secret> = Api::namespaced(client.clone(), &namespace);
+            secrets_namespaced
+                .delete("test-tls", &DeleteParams::default())
+                .await
+                .expect("Unable to delete secret");
+        }
+    }
+}
+
+async fn add_certificate(client: &Client, file: &str) {
+    let file = std::fs::File::open(&Path::new(file)).expect("Unable to open certificate file");
+    let k_yaml: Secret =
+        serde_yaml::from_reader(file).expect("Unable to convert certificate file to yaml");
+    let secrets: Api<Secret> = Api::all(client.clone());
+    let lp = ListParams::default();
+    let secret_list = secrets.list(&lp).await.expect("Unable to list secrets");
+}
+
+// async fn launch_app() -> thread::JoinHandle<> {
+//     // async fn launch_app() {
+//     thread::spawn(|| async {
+//         run().await;
+//     })
+// }
+
 #[tokio::test]
 async fn create_certificate() {
-    run().await;
+    let client = init_client().await;
+    delete_certificates(&client).await;
+    // launch_app();
+    let app = thread::spawn(|| async {
+        run().await;
+    });
+    add_certificate(&client, "certificate.yaml");
+    // run().await;
 }
